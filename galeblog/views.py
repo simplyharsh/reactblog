2391bc109cc5511cabcdbb0be6c37344da506072
Harsh Kohli <harsh@simplyharsh.com>
2016-03-15 19:23:29 +0530

    a basic homepage handler to test templates

---
+++ galeblog/views.py
@@ -0,0 +1,19 @@
+from django.views.generic.base import View
+from django.template import RequestContext
+from django.shortcuts import render_to_response as rtr
+
+class BaseHandler(View):
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super(BaseHandler, self).dispatch(request, *args, **kwargs)
+
+    def render_to_response(self, template_name, context=None):
+        if not isinstance(context, dict):
+            context = {}
+
+        return rtr(template_name, context,
+                   context_instance=RequestContext(self.request))
+
+class Home(BaseHandler):
+    def get(self, request):
+        return self.render_to_response('homepage.html')
