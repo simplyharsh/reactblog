51833df3288b715da1267d607ac2ec56f24c65fa
Harsh Kohli <harsh@simplyharsh.com>
2016-03-17 15:03:14 +0530

    Porting react component with actual data

--- app/components/ListItemWrapper.js
+++ app/components/ListItemWrapper.js
@@ -14,18 +14,20 @@ var ListItemWrapper = React.createClass({
 <article>
   <div className="blog-list-header">
     <div className="post-heading">
-      <h3><a href="#">This is an example of standard post format</a></h3>
+      <h3><a href="{article_url}" onClick={ this.onReadDetail }>{article.title}</a></h3>
     </div>
-    <img className="img-responsive" src="{% static 'img/hero-placehold.png' %}" alt="" />
+    <div className="hero-div">
+      <img className="img-responsive" src={ article.hero } alt="" />
+    </div>
   </div>
   <p>
-    Qui ut ceteros comprehensam. Cu eos sale sanctus eligendi, id ius elitr saperet, ocurreret pertinacia pri an. No mei nibh consectetuer, semper laoreet perfecto ad qui, est rebum nulla argumentum ei. Fierent adipisci iracundia est ei, usu timeam persius ea. Usu ea justo malis, pri quando everti electram ei, ex homero omittam salutatus sed.
+    { article.body }
   </p>
   <div className="blog-list-footer">
     <ul className="meta-post">
-      <li><span className="glyphicon glyphicon-calendar"></span><a href="#"> Mar 23, 2013</a></li>
-      <li><span className="glyphicon glyphicon-user"></span><a href="#"> Admin</a></li>
-      <li><span className="glyphicon glyphicon-folder-open"></span><a href="#"> Blog</a></li>
+      <li><span className="glyphicon glyphicon-calendar"></span>{ article.publication_date }</li>
+      <li><span className="glyphicon glyphicon-user"></span>{ article.author_name }</li>
+      <li><span className="glyphicon glyphicon-folder-open"></span>{ article.category_name || 'Uncategorized' }</li>
       <li><span className="glyphicon glyphicon-tags"></span><a href="#">4 Tags</a></li>
     </ul>
     <a href={article_url} className="read-this" onClick={ this.onReadDetail }><span className="glyphicon glyphicon-menu-right"></span>Read This Article</a>
