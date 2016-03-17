cf6df2a231c8b7e103e7faa450161db650ddeb50
Harsh Kohli <harsh@simplyharsh.com>
2016-03-17 16:46:16 +0530

    A little hack to unmount the current components on redirect

--- app/init.js
+++ app/init.js
@@ -7,15 +7,20 @@ var ArticleDetailWrapper = require('./components/ArticleDetailWrapper');
 var BlogReactor = {
   render_blog_list: function (page) {
     var page = page || 1;
+    var wrapper = document.getElementById('content-wrapper');
+    $('html, body').animate({ scrollTop: 0 }, 'fast');
     ReactDOM.render(
         <ArticleListWrapper page={page} />,
-      document.getElementById('content-wrapper')
+      wrapper
     );
   },
   render_blog_detail: function (blog_slug) {
+    var wrapper = document.getElementById('content-wrapper');
+    wrapper.innerHTML = '';
+    $('html, body').animate({ scrollTop: 0 }, 'fast');
     ReactDOM.render(
         <ArticleDetailWrapper slug={blog_slug} />,
-      document.getElementById('content-wrapper')
+      wrapper
     );
   }
 }
