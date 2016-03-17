0060af08f82aa7f653d805ca0e053541f2157b59
Harsh Kohli <harsh@simplyharsh.com>
2016-03-17 10:24:55 +0530

    trying a entry point for reactor

---
+++ app/init.js
@@ -0,0 +1,10 @@
+var BlogReactor = {
+  render_blog_detail: function (blog_slug) {
+    console.log('render blog_detail')
+  },
+  render_blog_list: function (blog_list) {
+    console.log('render blog_list')
+  }
+}
+
+module.exports = BlogReactor;
