3b5648a620c6ba46cb50dc11201fa09ede0806e8
Harsh Kohli <harsh@simplyharsh.com>
2016-03-17 15:04:35 +0530

    settings to serve uploaded files and datetime format settings

--- galeblog/settings.py
+++ galeblog/settings.py
@@ -125,14 +125,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+UPLOADS_DIR = os.path.join(BASE_DIR, 'uploads')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    UPLOADS_DIR
 ]
 
-UPLOADS_DIR = os.path.join(BASE_DIR, 'uploads')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DATE_FORMAT': '%d/%m/%Y',
+    'DATETIME_FORMAT': '%d %b, %Y at %I:%M %p'
 }
