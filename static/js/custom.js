52b4254d3573a8640a8c632e4a06935c2885a0da
Harsh Kohli <harsh@simplyharsh.com>
2016-03-17 17:39:11 +0530

    the js router to handle the paginations

--- static/js/custom.js
+++ static/js/custom.js
@@ -6,6 +6,9 @@ Router
   .add(/articles$/, function() {
     BlogReactor.render_blog_list();
   })
+  .add(/articles\/page\/(\d+)$/, function() {
+    BlogReactor.render_blog_list.apply(BlogReactor, arguments);
+  })
   .add(/article\/(.*)$/, function() {
     BlogReactor.render_blog_detail.apply(BlogReactor, arguments);
   })
