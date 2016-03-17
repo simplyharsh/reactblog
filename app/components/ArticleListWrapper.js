7fa5e649d31ffb8b8250aac39ee10f5a3a220562
Harsh Kohli <harsh@simplyharsh.com>
2016-03-17 17:38:11 +0530

    Changes in List reactor to accomodate pagination

--- app/components/ArticleListWrapper.js
+++ app/components/ArticleListWrapper.js
@@ -9,12 +9,22 @@ var ArticleListWrapper = React.createClass({
 
   componentDidMount: function() {
     var sui = this;
+    var page = this.props.page;
+    if (isNaN(Number(page))) {
+      page = 1;
+    } else {
+      page = Number(page);
+    }
+
     sui.serverRequest = $.ajax({
-      url: '/api/articles/',
+      url: '/api/articles/?page='+page,
       type: 'get',
       success: function (data) {
         sui.setState({
-          'article_items': data
+          'count': data.count,
+          'article_items': data.results,
+          'next': data.next,
+          'previous': data.previous
         })
       }
     });
@@ -24,14 +34,58 @@ var ArticleListWrapper = React.createClass({
     this.serverRequest.abort();
   },
 
+  page_redirect: function (page) {
+    if (isNaN(Number(page))) {
+      page = 1;
+    } else {
+      page = Number(page);
+    }
+    var url;
+    if (page <= 1) {
+      url = '/articles/';
+    } else {
+      url = '/articles/page/'+page+'/';
+    }
+    Router.redirect(url);
+  },
+
+  onNext: function (e) {
+    this.page_redirect(this.state.next);
+    e.preventDefault();
+  },
+
+  onPrev: function (e) {
+    this.page_redirect(this.state.previous);
+    e.preventDefault();
+  },
+
   render: function () {
     var article_items = this.state.article_items;
+    var next = this.state.next;
+    var prev = this.state.previous;
+    console.log('a', this.props.page);
+
+    var next_link = '', prev_link = '', next_prev = '';
+    if (next) {
+      var next_url = "/articles/page/"+next;
+      next_link = <a href={next_url} className="pull-right" onClick={ this.onNext }>Read Next Article <span className="glyphicon glyphicon-menu-right"></span></a>;
+    }
+    if (prev) {
+      var prev_url = "/articles/page/"+prev;
+      prev_link = <a href={prev_url} onClick={ this.onPrev }><span className="glyphicon glyphicon-menu-left"></span> Read Previous Article</a>;
+    }
+
+    if (next || prev) {
+      next_prev = <article>{prev_link}{next_link}<div className="clearfix"></div></article>
+    }
+
     var page = this.props.page;
     return (
       <div>
       {article_items.map(function (article_item) {
         return <ListItemWrapper key={article_item.id} article={article_item} />
       })}
+      { next_prev }
       </div>
     )
   }
