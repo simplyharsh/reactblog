53871756c59ce5693d3e63634fd5576f079cc25e
Harsh Kohli <harsh@simplyharsh.com>
2016-03-17 12:58:02 +0530

    Hooking react components with BlogReactor

--- app/init.js
+++ app/init.js
@@ -1,9 +1,22 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var BlogListWrapper = require('./components/BlogListWrapper');
+var BlogDetailWrapper = require('./components/BlogDetailWrapper');
+
 var BlogReactor = {
   render_blog_detail: function (blog_slug) {
-    console.log('render blog_detail')
+    ReactDOM.render(
+        <BlogDetailWrapper slug={blog_slug} />,
+      document.getElementById('content-wrapper')
+    );
   },
   render_blog_list: function (blog_list) {
-    console.log('render blog_list')
+    var blog_items = [{'id': 'a'}, {'id': 'b'}];
+    ReactDOM.render(
+        <BlogListWrapper blog_items={blog_items} />,
+      document.getElementById('content-wrapper')
+    );
   }
 }
 
