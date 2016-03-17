8df363f6e4134bfc58512e44c657931fb0dc62a5
Harsh Kohli <harsh@simplyharsh.com>
2016-03-17 16:45:33 +0530

    RandomArticles React ajax call

--- app/components/RandomArticlesWrapper.js
+++ app/components/RandomArticlesWrapper.js
@@ -2,18 +2,45 @@ var React = require('react');
 var RandomArticlesItemWrapper = require('./RandomArticlesItemWrapper');
 
 var RandomArticlesWrapper = React.createClass({
+  getInitialState: function () {
+    return {
+      'article_items': []
+    }
+  },
+
+  componentDidMount: function() {
+    var sui = this;
+    sui.serverRequest = $.ajax({
+      url: '/api/random_articles/',
+      type: 'get',
+      success: function (data) {
+        sui.setState({
+          'article_items': data
+        })
+      }
+    });
+  },
+
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
+
   render: function () {
-    var article_items = [{'id': 1}, {'id': 2}];
+    var slug = this.props.slug;
+    var article_items = this.state.article_items;
     return (
   <article>
     <p className="lead">What to read next?</p>
     <hr />
     <div className="row">
-      {article_items.map(function (article_item) {
-        return <RandomArticlesItemWrapper key={article_item.id} article={article_item} />
+        {article_items.map(function (article_item, iter) {
+          if (article_item.slug != slug && iter < 4) {
+            return <RandomArticlesItemWrapper key={article_item.id} article={article_item} />
+          } else {
+            return '';
+          }
       })}
     </div>
-        <div className="cleafix"></div>
   </article>
    )}
 });
